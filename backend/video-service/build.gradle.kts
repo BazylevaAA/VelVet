4867121ff9777ae6c12eda36e91b304d123fcb0b
BazylevaAA <aab.nn16@gmail.com>
2026-05-03 22:26:03 +0300

    Настройка окружения микросервисов и исправление багов

--- backend/video-service/build.gradle.kts
+++ backend/video-service/build.gradle.kts
@@ -34,8 +34,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-server-call-logging")
-    implementation("io.ktor:ktor-server-multipart")
-    implementation("io.ktor:ktor-server-status-pages")
+implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-cors")
 
     // --- JWT (проверяем токен от auth-service) ---
